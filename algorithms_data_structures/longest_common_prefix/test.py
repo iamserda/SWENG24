582dc93291522a91ce7e28dd783c5acefb3d4f87
iamserda <yiamserda@gmail.com>
2024-01-08 16:04:11 -0500

    separated problem, solution, and test to improve future practice and ease of use. -@iamserda

---
+++ algorithms_data_structures/longest_common_prefix/test.py
@@ -0,0 +1,29 @@
+import unittest
+from main import longest_common_prefix
+# from solution import longest_common_prefix
+
+
+class TestLongestCommonPrefix(unittest.TestCase):
+
+    def test_basic_functionality(self):
+        self.assertEqual(longest_common_prefix(
+            ["flower", "flow", "flight"]), "fl")
+        self.assertEqual(longest_common_prefix(["dog", "racecar", "car"]), "")
+
+    def test_empty_list(self):
+        self.assertEqual(longest_common_prefix([]), "")
+
+    def test_single_string(self):
+        self.assertEqual(longest_common_prefix(["alone"]), "alone")
+
+    def test_no_common_prefix(self):
+        self.assertEqual(longest_common_prefix(["abc", "def", "ghi"]), "")
+
+    def test_varied_length_strings(self):
+        self.assertEqual(longest_common_prefix(["a", "ab", "abc"]), "a")
+        self.assertEqual(longest_common_prefix(
+            ["longest", "longer", "long"]), "long")
+
+
+if __name__ == "__main__":
+    unittest.main()
