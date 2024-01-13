d0c8a2a7bd3e1901b016d0248a8609becc4ea00f
iamserda <yiamserda@gmail.com>
2024-01-13 03:48:46 -0500

    new features: 3 card factories(amex, visa, master) and a selector/generator -@iamserda

--- card_issuer/main.py
+++ card_issuer/main.py
@@ -16,3 +16,40 @@ def select_card(pmt_servicer: str) -> str:
     }
 
     return generate_card(pmt_servicer, servicers_tools[pmt_servicer][0], servicers_tools[pmt_servicer][1])
+
+
+def generate_card(svc, card_genrator, verifier):
+    """generates a new card based on servicer:str, card_generator:def, verifier:set"""
+    new_card = card_genrator()
+    while new_card in verifier:
+        new_card = card_genrator()
+    return new_card
+
+
+def amex_factory():
+    """generates a fake amex card"""
+    import random
+    amex_id = random.randint(32000, 85000)
+    group_nums = random.randint(312123, 985430)
+    customer_nums = random.randint(32000, 85000)
+    return f"{amex_id}-{group_nums}-{customer_nums}"
+
+
+def visa_factory():
+    """generates a fake visa card"""
+    import random
+    visa_id = random.randint(4100, 4999)
+    bank_id = random.randint(1111, 9000)
+    group_id = random.randint(1110, 9900)
+    client_id = random.randint(1100, 9990)
+    return f"{visa_id}-{bank_id}-{group_id}-{client_id}"
+
+
+def master_factory():
+    """ a fake master card generator"""
+    import random
+    issuer_id = random.randint(5100, 5999)
+    group_id1 = random.randint(1111, 9999)
+    group_id2 = random.randint(1111, 9999)
+    customer_id = random.randint(1111, 9999)
+    return f"{issuer_id}-{group_id1}-{group_id2}-{customer_id}"
