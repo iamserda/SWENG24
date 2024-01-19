0789cf003a4f9b7fa8e881b61f338b63f93d5292
iamserda <yiamserda@gmail.com>
2024-01-18 20:58:35 -0500

    py operators: -, +, *, /, //, and, or, lt, gt, equal, lt or equal, gt or equal. -@iamserda

---
+++ python_fundamentals/operators_and_boolean.py
@@ -0,0 +1,62 @@
+# this is a single-line comment
+"""this is
+a multiline comment.
+also known as docstring as in documentation strings"""
+
+# addition
+print(1 + 1)
+
+# subtraction
+print(2 * 41)
+
+# multiplication
+print(1 - 1)
+
+# division
+print(10 / 5)
+
+# strictly-integer division
+print(10 // 5)
+
+# modulo arithmetic division
+print(10 % 5)
+
+# boolean: True, False
+a = True
+b = False
+print(a)
+print(b)
+a = 0
+b = 1
+b = -1
+
+# 0 is the False(y) value for numbers
+print(bool(a))
+# any number other than 0 is True
+print(bool(a))
+print(bool(b))
+
+# equality, less-than, greater-than,
+# less-than-or-equal, greater-than-or-equal
+
+# equality operator ==
+print(1 == 1)  # True
+a = 10
+b = 20
+print(a == b)  # False
+
+# less-than <
+print("7 < 5:", 7 < 5)  # False
+print("7 < 15", 7 < 15)  # True
+
+# less-than or equal <=
+print("7 <= 5", 7 <= 5)  # False
+print("5 <= 5", 5 <= 5)  # True
+
+# greater-than >
+print("7 > 5", 7 <= 5)  # True
+print("7 > 15", 7 <= 15)  # False
+
+# greater-than or equal
+print("1 >= 1", 1 >= 1)  # True
+print("1 >= 10", 1 >= 10)  # False
