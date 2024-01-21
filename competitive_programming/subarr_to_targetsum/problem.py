95fe1c15dc21d8c5844c7cd061314a5e32082ce1
@iamserda <iamserda@gmail.com>
2024-01-21 01:23:33 -0500

    minor-mod: change names of fucntions to abide by linter rules. - @iamserda

--- competitive_programming/subarr_to_targetsum/problem.py
+++ competitive_programming/subarr_to_targetsum/problem.py
@@ -1,7 +1,8 @@
+# importing unittest in order to test solution.
 import unittest
 
 
-def subarraysum_to_targetvalue(my_list: list, target_value: int) -> list:
+def sum_to_target(my_list: list, target_value: int) -> list:
     """
         :type strs: List[str]
         :rtype: str
@@ -13,40 +14,49 @@ def subarraysum_to_targetvalue(my_list: list, target_value: int) -> list:
 
 
 # TEST ARENA
-class TestingSubArraySumToTargetVal(unittest.TestCase):
-    def test_none_matching_subarr1(self):
+class TestingSumToTarget(unittest.TestCase):
+    """PROTO: TODO"""
+
+    def test_none_matching_sub_arr1(self):
         """ input: list, """
-        self.assertEqual(subarr_sum_to_target_val(
+        self.assertEqual(sum_to_target(
             my_list=[8, 4, 5, 0, 3, 2, 1, 6, 7, 9], target_value=16), [])
 
-    def test_none_matching_subarr2(self):
-        self.assertEqual(subarr_sum_to_target_val(
+    def test_none_matching_sub_arr2(self):
+        """PROTO: TODO"""
+        self.assertEqual(sum_to_target(
             my_list=[8, 4, 5, 0, 3, 2, 1, 6, 7, 9], target_value=13), [])
 
-    def test_found_matching_sub_arr1(self):
-        self.assertEqual(subarr_sum_to_target_val(
+    def test_found_matching_sub_arr3(self):
+        """PROTO: TODO"""
+        self.assertEqual(sum_to_target(
             [8, 4, 5, 0, 3, 2, 1, 6, 7, 9], 6), [3, 6], "✅!")
 
-    def test_found_matching_sub_arr2(self):
-        self.assertEqual(subarr_sum_to_target_val(
+    def test_found_matching_sub_arr4(self):
+        """PROTO: TODO"""
+        self.assertEqual(sum_to_target(
             [8, 4, 5, 0, 3, 2, 1, 6, 7, 9], 10), [2, 5], "✅!")
 
     # IF ALL above passed!, activate the ones below:
     # RAINY DAY TESTS:
     # def test_target_arg_is_nonetype(self):
-    #     self.assertEqual(subarr_sum_to_target_val(
+        """PROTO: TODO"""
+    #   self.assertEqual(sum_to_target(
     #         my_list=[8, 4, 5, 0, 3, 2, 1, 6, 7, 9], target_value=None), "✅!")
 
     # def test_list_arg_is_nonetype(self):
-    #     self.assertEqual(subarr_sum_to_target_val(
-    #         my_list=None, target_value=10), [], "✅!")
+    #   """PROTO: TODO"""
+    #   self.assertEqual(sum_to_target(
+    #   my_list=None, target_value=10), [], "✅!")
 
     # def test_list_arg_is_empty(self):
-    #     self.assertEqual(subarr_sum_to_target_val(
+    #   """PROTO: TODO"""
+    #   self.assertEqual(sum_to_target(
     #         my_list=[], target_value=10), [], "✅!")
 
-    # def test_list_arg_isnot_integer(self):
-    #     self.assertEqual(subarr_sum_to_target_val(
+    # def test_list_arg_is_not_integer(self):
+    #   """PROTO: TODO"""
+    #     self.assertEqual(sum_to_target(
     #         my_list=[8, 4, 5, 0, 3, 2, 1, 6, 7, 9], target_value="Hello",),  "✅!")
 
 
