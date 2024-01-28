b1060e1f9e5b6f48de628454ad4f866645862fc7
@iamserda <iamserda@gmail.com>
2024-01-28 03:06:38 -0500

    configured challenge.py

---
+++ py_core/odd_even_sums/challenge.py
@@ -0,0 +1,11 @@
+""" Given an integer, return the sum of its even digits a sum of its odd digits"""
+
+
+def sum_of_odd_and_even(num):
+    """ Given an integer, return the sum of its even digits a sum of its odd digits"""
+
+
+# DO NOT CHANGE THE CODE BELOW...
+# Tests Arena:
+assert sum_of_odd_and_even(12345) == (9, 6)
+assert sum_of_odd_and_even(1357902468) == (25, 20)
