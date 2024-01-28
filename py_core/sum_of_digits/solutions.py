163dd763db2aa113eb93bd9c5f29a6309ec4ff4b
@iamserda <iamserda@gmail.com>
2024-01-28 01:13:38 -0500

    minor: linting related cleanup.-@iamserda

--- py_core/sum_of_digits/solutions.py
+++ py_core/sum_of_digits/solutions.py
@@ -1,16 +1,20 @@
+""" Given an integer, return its individual digits. Do not convert to str."""
 def sum_of_digits(some_number):
-    sum = None
+    """ Given an integer, return its individual digits."""
+    the_sum = None
     if isinstance(some_number, int):
-        sum = 0
+        the_sum = 0
         while some_number != 0:
             remainder_num = some_number % 10
             some_number = some_number // 10
-            sum += remainder_num
-    print(sum)
-    return sum
+            the_sum += remainder_num
+    return the_sum
 
-
+# Tests Arena:
 assert sum_of_digits(12345) == 15
 assert sum_of_digits(1) == 1
 assert sum_of_digits(345) == 12
 assert sum_of_digits(0) == 0
+
+# value = sum_of_digits(12345)
+# print(value)
