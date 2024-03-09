58f298d787a78622fb88dc7047314a5a058d0106
@iamserda <iamserda@gmail.com>
2024-03-08 21:55:57 -0500

    fix: In cases where there are two middle elements, ex when total number of elements is even, I am returning both as a tuple. -@iamserda

--- py_dsa/04_linked_lists/exercises/001_find_middle_node.py
+++ py_dsa/04_linked_lists/exercises/001_find_middle_node.py
@@ -27,25 +27,36 @@ class LinkedList:
         :type head: ListNode
         :rtype: ListNode
         """
-        fast = self.head
         slow = self.head
+        fast = slow.next
         while fast and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
-
+        if fast:
+            return slow, slow.next
         return slow
 
 
 
-
+# TESTING AREANAS: 
+# For EVEN size linked-list, mid is both len(LL)/2 and len(LL)/2 - 1.
+# Here, we are using the first N/2 - 1. 
+# But be mindful that in cases of even numbers of elements, 
+# the element at index: N/2 -1 and the element at index: N/2 are BOTH middle elements.
 my_linked_list = LinkedList(1)
+assert my_linked_list.find_middle_node().value == 1
 my_linked_list.append(2)
+assert [x.value for x in my_linked_list.find_middle_node()] == [1,2]
 my_linked_list.append(3)
+assert my_linked_list.find_middle_node().value == 2
 my_linked_list.append(4)
+assert [x.value for x in my_linked_list.find_middle_node()] == [2,3]
 my_linked_list.append(5)
-
-# Testing Arenas
 assert my_linked_list.find_middle_node().value == 3
+my_linked_list.append(6)
+assert [x.value for x in my_linked_list.find_middle_node()] == [3,4]
+my_linked_list.append(7)
+assert my_linked_list.find_middle_node().value == 4
 
 
 """
