1daa9b47399cba9b636b300b8a8b7d7df21f1a2c
@iamserda <iamserda@gmail.com>
2024-03-09 16:30:38 -0500

    solved: added solutions to finding kth node from end. -@iamserda

---
+++ py_dsa/04_linked_lists/exercises/003_find_kth_from_end.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+
+    def find_kth_from_end(self, k):
+        fast = self.head
+        slow = self.head
+
+        if self.head is None:
+            return None
+
+        for i in range(1, k):
+            fast = fast.next
+            if fast is None:
+                return None
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        return slow
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+
+k = 2
+assert my_linked_list.find_kth_from_end(k).value == 4
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    4
+    
+"""
