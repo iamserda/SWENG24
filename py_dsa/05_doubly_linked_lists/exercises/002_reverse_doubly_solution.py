45ec0755a3c0a2f8f97b6e807562184ab0726d0e
@iamserda <iamserda@gmail.com>
2024-03-13 21:17:07 -0400

    lved: reverse doubly-linked-list -@iamserda

---
+++ py_dsa/05_doubly_linked_lists/exercises/002_reverse_doubly_solution.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def to_list(self):
+        arr = []
+        temp = self.head
+        while temp is not None:
+            arr.append(temp.value)
+            temp = temp.next
+        return arr
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+    def reverse(self):
+        if not self.head:
+            return
+        temp = self.head
+        while temp:
+            temp.next, temp.prev = temp.prev, temp.next
+            temp = temp.prev
+        self.head, self.tail = self.tail, self.head
+
+doubly = DoublyLinkedList(1)
+doubly.append(2)
+doubly.append(3)
+doubly.append(4)
+doubly.append(5)
+assert doubly.to_list() == [1,2,3,4,5]
+doubly.reverse()
+assert doubly.to_list() == [5,4,3,2,1]
