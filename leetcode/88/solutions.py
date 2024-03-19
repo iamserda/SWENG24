353e49fbb694cd888ccff3f4c88a3ad4a228a6eb
@iamserda <iamserda@gmail.com>
2024-03-19 15:43:13 -0400

    solved: LC88, see readme, solutions-explained, etc... -@iamserda

--- leetcode/88/solutions.py
+++ leetcode/88/solutions.py
@@ -1,33 +1,57 @@
 def merge(nums1, m, nums2, n):
-    """
-    :type nums1: List[int]
-    :type m: int
-    :type nums2: List[int]
-    :type n: int
-    :rtype: None Do not return anything, modify nums1 in-place instead.
-    """
-    nums1_indexer = m - 1
-    nums2_indexer = n - 1
-    next_indexer = m + n - 1
-    if n - 1 < 0:
-        return
+    a = m + n
+    m_idx = m-1
+    n_idx = n-1
+    
+    while a >= 0:
+        a -= 1
+        print(a, m_idx, n_idx)
+        if n_idx < 0:
+            nums1[a] = nums1[m_idx]
+            if m_idx >= 0:
+                m_idx -= 1
+            continue
 
-    while next_indexer >= 0:
-        if nums1[nums1_indexer] >= nums2[nums2_indexer]:
-            nums1[next_indexer] = nums1[nums1_indexer]
-            nums1_indexer -= 1
+        if m_idx < 0:
+            nums1[a] = nums2[n_idx]
+            if n_idx >= 0:
+                n_idx -= 1
+            continue
+
+        if nums1[m_idx] >= nums2[n_idx]:
+            nums1[a] = nums1[m_idx]
+            m_idx -= 1
         else:
-            nums1[next_indexer] = nums2[nums1_indexer]
-            nums2_indexer -= 1
-        next_indexer -= 1
-        if nums1_indexer < 0 or nums2_indexer < 0:
-            break
+            nums1[a] = nums2[n_idx]
+            n_idx -= 1
+    
+    print(nums1)
 
 
 # Testing:
-nums1 = [0]
-nums2 = [1]
+# Testlast_indexng Arena:
+# Test 0:
+nums1 = [2,3,0,0,0]
+nums2 = [0,1,3]
+M = 2
+N = 3
+print(f"Before: {nums1}, {nums2}")
+merge(nums1=nums1, m=M, nums2=nums2, n=N)
+print(f"After: {nums1}")
+
+# Test 1:
+nums1 = [2]
+nums2 = []
 M = 1
+N = 0
+print(f"Before: {nums1}")
+merge(nums1=nums1, m=M, nums2=nums2, n=N)
+print(f"After: {nums1}")
+
+# Test 2:
+nums1 = []
+nums2 = [1]
+M = 0
 N = 1
 print(f"Before: {nums1}")
 merge(nums1=nums1, m=M, nums2=nums2, n=N)
