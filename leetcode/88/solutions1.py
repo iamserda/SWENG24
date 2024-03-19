dd8b857138194ce1810f297fc4bae5d35c4c1862
@iamserda <iamserda@gmail.com>
2024-03-19 15:59:26 -0400

    solved: LC88, see readme, solutions-explained, etc... -@iamserda

---
+++ leetcode/88/solutions1.py
@@ -0,0 +1,31 @@
+def merge(nums1, m, nums2, n):
+
+
+# Testlast_indexng Arena:
+
+# Test 0:
+nums1 = [2,3,0,0,0]
+nums2 = [0,1,3]
+M = 2
+N = 3
+print(f"Before: {nums1}, {nums2}")
+merge(nums1=nums1, m=M, nums2=nums2, n=N)
+print(f"After: {nums1}")
+
+# # Test 1:
+nums1 = [2]
+nums2 = []
+M = 1
+N = 0
+print(f"Before: {nums1}")
+merge(nums1=nums1, m=M, nums2=nums2, n=N)
+print(f"After: {nums1}")
+
+# # Test 2:
+nums1 = [2]
+nums2 = [0,1,3]
+M = 2
+N = 3
+print(f"Before: {nums1}")
+merge(nums1=nums1, m=M, nums2=nums2, n=N)
+print(f"After: {nums1}")
