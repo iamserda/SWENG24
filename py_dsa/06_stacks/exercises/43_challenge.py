b1b70a05a86182a4a913cb6ef8be7bc8b6541cb1
@iamserda <iamserda@gmail.com>
2024-03-22 22:18:22 -0400

    solved: build stack using list. -@iamserda

---
+++ py_dsa/06_stacks/exercises/43_challenge.py
@@ -0,0 +1,110 @@
+"""DO NOT CHANGE ANY CODE WITHIN CLASS STACK BELOW"""
+class Stack:
+    """DO NOT CHANGE ANY CODE WITHIN CLASS STACK"""
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+"""DO NOT CHANGE ANY CODE WITHIN CLASS STACK ABOVE"""
+
+
+"""is_balanced_parentheses STARTS HERE"""
+def is_balanced_parentheses(a_str):
+    """Start Here"""
+
+"""is_balanced_parentheses END HERE"""
+
+
+# Testing Arenas:
+
+def test_is_balanced_parentheses():
+    try:
+        assert is_balanced_parentheses('((()))') == True
+        print('Test case 1 passed')
+    except AssertionError:
+        print('Test case 1 failed')
+
+    try:
+        assert is_balanced_parentheses('()') == True
+        print('Test case 2 passed')
+    except AssertionError:
+        print('Test case 2 failed')
+
+    try:
+        assert is_balanced_parentheses('(()())') == True
+        print('Test case 3 passed')
+    except AssertionError:
+        print('Test case 3 failed')
+
+    try:
+        assert is_balanced_parentheses('(()') == False
+        print('Test case 4 passed')
+    except AssertionError:
+        print('Test case 4 failed')
+
+    try:
+        assert is_balanced_parentheses('())') == False
+        print('Test case 5 passed')
+    except AssertionError:
+        print('Test case 5 failed')
+
+    try:
+        assert is_balanced_parentheses(')(') == False
+        print('Test case 6 passed')
+    except AssertionError:
+        print('Test case 6 failed')
+
+    try:
+        assert is_balanced_parentheses('') == True
+        print('Test case 7 passed')
+    except AssertionError:
+        print('Test case 7 failed')
+
+    try:
+        assert is_balanced_parentheses('()()()()') == True
+        print('Test case 8 passed')
+    except AssertionError:
+        print('Test case 8 failed')
+
+    try:
+        assert is_balanced_parentheses('(())(())') == True
+        print('Test case 9 passed')
+    except AssertionError:
+        print('Test case 9 failed')
+
+    try:
+        assert is_balanced_parentheses('(()()())') == True
+        print('Test case 10 passed')
+    except AssertionError:
+        print('Test case 10 failed')
+
+    try:
+        assert is_balanced_parentheses('((())') == False
+        print('Test case 11 passed')
+    except AssertionError:
+        print('Test case 11 failed')
+
+test_is_balanced_parentheses()
