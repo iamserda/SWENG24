9331899706f79f89f96bec8e48e8c9de720867a1
@iamserda <iamserda@gmail.com>
2024-03-22 22:56:55 -0400

    solved: build stack using a Singly-Linked-List. @iamserda

--- py_dsa/06_stacks/core/Stack.py
+++ py_dsa/06_stacks/core/Stack.py
@@ -2,16 +2,25 @@ from Node import Node
 
 class Stack:
     """Linked-List-based Stack"""
-    def __init__(self):
-        self.top = None
+    def __init__(self, value=None):
+        if value is None:
+            self.top = None
+            self.height = 0
+        else:
+            new_node = Node(value)
+            self.top = new_node
+            self.height = 1
 
     def insert(self, value):
+        if value is None:
+            return
         new_node = Node(value)
         if not self.top:
             self.top = new_node
         else:
             new_node.next = self.top
             self.top = new_node
+        self.height += 1
         return self.top
     
     def pop(self):
@@ -20,6 +29,7 @@ class Stack:
         temp = self.top
         self.top = self.top.next
         temp.next = None
+        self.height -= 1
         return temp
     
     def stack_to_arr(self):
@@ -40,4 +50,4 @@ new_stack.insert(40)
 new_stack.pop() # 40
 new_stack.insert(50)
 new_stack_arr = new_stack.stack_to_arr() # [50,30,10]
-print(new_stack_arr)
+print(new_stack_arr, new_stack.height)
